5ca782e7ef1bed4588148c24e4bb43889f767c30
prashant226 <prashantnaraniya2003@gmail.com>
2024-01-03 12:07:47 +0530

    commited

--- CLASS-02/index.js
+++ CLASS-02/index.js
@@ -1,5 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+/**
+ * PARCEL FEATURES:
+ * BUILDING
+ * MINIFY
+ * CLEANING AND CODE
+ * DEV AND PRODUCTION FRIENDLY
+ * SUPER-FAST TIME SHORT
+ * caching while devlopment
+ * compatable with older version of the browser
+ * HTTPS on dev 
+ * port number
+ * 
+ */
 
 const heading = React.createElement(
     "h1", {
