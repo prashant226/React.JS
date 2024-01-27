3f0fa49e2362010be477ce0084ccbd4257b9228a
prashant226 <prashantnaraniya2003@gmail.com>
2024-01-27 09:06:48 +0530

    commited

--- restart.react/app.js
+++ restart.react/app.js
@@ -1,3 +1,17 @@
-const heading = React.createElement("h1", { id: "heading" }, "hello from js");
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "i'm an h2 tag"),
+
+    ]),
+
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag"),
+
+    ]),
+
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
